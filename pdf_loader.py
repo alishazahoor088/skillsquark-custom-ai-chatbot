a220570f2aeba7b89270eb09763d342a6bd2e1a9
alishazahoor088 <alishazahoor088@gmail.com>
2026-07-09 17:29:07 +0500

    Add pdf_loader.py

--- pdf_loader.py
+++ pdf_loader.py
@@ -1,1 +1,10 @@
+from langchain_community.document_loaders import PyPDFLoader
 
+
+def load_pdf(file_path):
+    """
+    Load a PDF file and return its pages as LangChain documents.
+    """
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+    return documents
